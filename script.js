7b9a3362ac4dd00a97db791125964522bf89620f
sibaram626 <sibaram626@gmail.com>
2023-07-18 17:23:54 +0530

    i have added my github link and added hover border

--- script.js
+++ script.js
@@ -1,9 +1,101 @@
-const navbar = document.getElementById("navbar");
 
-navbar.addEventListener("mouseover", function() {
-  this.style.animation = "flashing 0.5s infinite";
-});
 
-navbar.addEventListener("mouseout", function() {
-  this.style.animation = "";
-});
+// navbar.addEventListener("mouseover", function() {
+//   this.style.animation = "flashing 0.5s infinite";
+// });
+
+// navbar.addEventListener("mouseout", function() {
+//   this.style.animation = "";
+
+// });
+
+
+
+
+
+// <!-- <!DOCTYPE html>
+// <html>
+// <head>
+//   <title>Event Handlers Example</title>
+// </head>
+// <body>
+//   <h1>Event Handlers Example</h1>
+
+//   <div onmouseover="handleMouseOver()" onmouseout="handleMouseOut()" style="background-color: lightgray; padding: 20px;">
+//     Hover over me (onmouseover, onmouseout)
+//   </div>
+
+//   <p id="output"></p>
+
+//   <script>
+//     function handleMouseOver() {
+//       updateOutput('Mouse over');
+//     }
+
+//     function handleMouseOut() {
+//       updateOutput('Mouse out');
+//     }
+
+//     function updateOutput(message) {
+//       var outputElement = document.getElementById('output');
+//       outputElement.textContent = message;
+//     }
+//   </script>
+// </body>
+// </html> -->
+
+
+// <!DOCTYPE html>
+// <html>
+// <head>
+//   <title>Event Handlers Example</title>
+// </head>
+// <body>
+//   <h1>Event Handlers Example</h1>
+
+//   <button onclick="handleClick()">Click Me (onclick)</button>
+
+//   <input type="text" oninput="handleInput(event)" placeholder="Type Something (oninput)">
+
+//   <div onmouseover="handleMouseOver()" onmouseout="handleMouseOut()" style="background-color: rgb(85, 236, 25); padding: 20px;">
+//     Hover over me (onmouseover, onmouseout)
+//   </div>
+
+//   <form onsubmit="handleSubmit(event)">
+//     <input type="submit" value="Submit (onsubmit)">
+//   </form>
+
+//   <div id="output">outputElement</div>
+
+//   <!-- <script>
+//     function handleClick() {
+//       appendMessage('Button clicked');
+//     }
+
+//     function handleInput(event) {
+//       appendMessage('Input value: ' + event.target.value);
+//     }
+
+//     function handleMouseOver() {
+//       appendMessage('Mouse over');
+//     }
+
+//     function handleMouseOut() {
+//       appendMessage('Mouse out');
+//     }
+
+//     function handleSubmit(event) {
+//       event.preventDefault();
+//       appendMessage('Form submitted');
+//     }
+
+//     function appendMessage(message) {
+//       var outputElement = document.getElementById('output');
+//       outputElement.innerHTML += message + '<br>';
+//     }
+//     console.log(message)
+//   </script> -->
+//   <script src="JS/script.js"></script>
+// </body>
+// </html>
+
